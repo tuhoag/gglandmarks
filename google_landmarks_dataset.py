162a3786c734b9b233620ddfdabf99d103a13db2
Tu Hoang <hatu87@gmail.com>
2018-05-14 20:16:12 +0200

    add multi process download

--- google_landmarks_dataset.py
+++ google_landmarks_dataset.py
@@ -4,6 +4,9 @@ from io import BytesIO
 from urllib import request, error
 import os
 import tensorflow as tf
+import multiprocessing
+from functools import reduce, partial
+
 #import keras.preprocessing.image as krimage
 
 DEFAULT_DIRECTORY='./data/landmarks_recognition/'
@@ -44,21 +47,58 @@ def load_local_images(directory, data):
 
     return images
 
+def split_chunks(data, num_processes):
+    total_images = data.shape[0]
+    chunk_size = int(total_images / num_processes)
+    chunks_data = [data.iloc[i:i + chunk_size] for i in range(0, total_images, chunk_size)]
+    chunks = [(i, chunks_data[i]) for i in range(len(chunks_data))]
+
+    print('number of processes: {}'.format(num_processes))
+    print('total number of images: {:,}'.format(total_images))
+    print('chunk size: {:,}'.format(chunk_size))
+    print('total chunks: {}'.format(len(chunks)))
+    print('data: {}'.format(data))
+    print('first chunk: {}'.format(chunks[0]))
+    
+    sum_chunk = 0
+    for i in range(0, len(chunks)):
+        print('chunk {} length: {}'.format(chunks[i][0], len(chunks[i][1])))
+        sum_chunk += len(chunks[i][1])
+    # sum_chunk = reduce(lambda x, acc: acc + len(x), chunks)    
+    print('sum chunks: {:,}'.format(sum_chunk))
+
+    assert(sum_chunk == data.shape[0])
+
+    return chunks
+
 def download(directory, data):
+    num_processes = multiprocessing.cpu_count()
+    chunks = split_chunks(data, num_processes)
+
+    
+    pool = multiprocessing.Pool(processes=num_processes)
+    prod = partial(download_chunk, directory=directory)
+    result = pool.map(prod, chunks)
+    print(result)
+    # image_paths = []
+        
+
+def download_chunk(chunk, directory):
+    chunk_index, data = chunk
+    print('start downloading chunk: {}'.format(chunk_index))
+    total_images = len(data)
+    count = 0
     image_paths = []
 
-    total_images = len(data)
-    print('start downloading images: {}'.format(total_images))
-    count = 0
     for _, row in data.iterrows():
         image_path = download_image(row['url'], directory, row['id'])
         if image_path != '':
             image_paths.append({'id': row['id'], 'path': image_path})
 
         count += 1
-        print('download {} / {} image - {:4.4f}%'.format(count, total_images, count/total_images*100))
+        print('[{}] - download {} / {} image - {:4.4f}%'.format(chunk_index, count, total_images, count/total_images*100))
 
-    return pd.Series(image_paths, name='path')
+    return image_paths
 
 def download_image(url, directory, name):
     image_path = os.path.join(directory, '{}.jpg'.format(name))
