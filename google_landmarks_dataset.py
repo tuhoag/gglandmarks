a1e222f0d8e52354db6085d91e637c9fbdc1e727
Tu Hoang <hatu87@gmail.com>
2018-05-14 22:01:08 +0200

    add resize

--- google_landmarks_dataset.py
+++ google_landmarks_dataset.py
@@ -12,27 +12,32 @@ from functools import reduce, partial
 DEFAULT_DIRECTORY='./data/landmarks_recognition/'
 DEFAULT_ORIGINAL_IMG_DIRECTORY='original'
 
-def load_data(directory=DEFAULT_DIRECTORY, force_download=True):
-    train = dataset(directory, 'train.csv', 'images', force_download)
-    test = pd.read_csv(directory + 'test.csv')
 
-    return train, test
+def load_train_dataset(directory=DEFAULT_DIRECTORY, resize=None, sample=False, force_download=True):
+    return dataset(directory, 'train.csv', resize, sample, force_download)
 
-def load_train_dataset(directory=DEFAULT_DIRECTORY, force_download=True):
-    return dataset(directory, 'train.csv', DEFAULT_ORIGINAL_IMG_DIRECTORY, force_download)
 
-def load_test_dataset(directory=DEFAULT_DIRECTORY, force_download=True):
-    return dataset(directory, 'test.csv', DEFAULT_ORIGINAL_IMG_DIRECTORY, force_download)
+def load_test_dataset(directory=DEFAULT_DIRECTORY, resize=None, sample=False, force_download=True):
+    return dataset(directory, 'test.csv', resize, sample, force_download)
 
-def dataset(directory, data_file, images_folder, force_download=True):
+def dataset(directory, data_file, resize, sample, force_download=True):
     df = pd.read_csv(os.path.join(directory, data_file))
-    images = load_images(os.path.join(directory, images_folder), df, force_download)
+
+    if sample:
+        df = df.head()
+
+    if resize is None:
+        images_folder = DEFAULT_ORIGINAL_IMG_DIRECTORY
+    else:
+        images_folder = '{}_{}'.format(resize[0], resize[1])
+
+    images = load_images(os.path.join(directory, images_folder), df, resize, force_download)
 
     return images, df['landmark_id'].values
 
-def load_images(directory, data, force_download):
+def load_images(directory, data, resize, force_download):
     if force_download:
-        image_paths = download(directory, data)
+        image_paths = download(directory, data, resize)
         # remove empty
 
     images = load_local_images(directory, data)
@@ -59,31 +64,30 @@ def split_chunks(data, num_processes):
     print('total chunks: {}'.format(len(chunks)))
     print('data: {}'.format(data))
     print('first chunk: {}'.format(chunks[0]))
-    
+
     sum_chunk = 0
     for i in range(0, len(chunks)):
         print('chunk {} length: {}'.format(chunks[i][0], len(chunks[i][1])))
         sum_chunk += len(chunks[i][1])
-    # sum_chunk = reduce(lambda x, acc: acc + len(x), chunks)    
+    # sum_chunk = reduce(lambda x, acc: acc + len(x), chunks)
     print('sum chunks: {:,}'.format(sum_chunk))
 
     assert(sum_chunk == data.shape[0])
 
     return chunks
 
-def download(directory, data):
+def download(directory, data, resize):
     num_processes = multiprocessing.cpu_count()
     chunks = split_chunks(data, num_processes)
 
-    
     pool = multiprocessing.Pool(processes=num_processes)
-    prod = partial(download_chunk, directory=directory)
+    prod = partial(download_chunk, directory=directory, resize=resize)
     result = pool.map(prod, chunks)
     print(result)
     # image_paths = []
-        
 
-def download_chunk(chunk, directory):
+
+def download_chunk(chunk, directory, resize):
     chunk_index, data = chunk
     print('start downloading chunk: {}'.format(chunk_index))
     total_images = len(data)
@@ -91,7 +95,7 @@ def download_chunk(chunk, directory):
     image_paths = []
 
     for _, row in data.iterrows():
-        image_path = download_image(row['url'], directory, row['id'])
+        image_path = download_image(row['url'], directory, row['id'], resize)
         if image_path != '':
             image_paths.append({'id': row['id'], 'path': image_path})
 
@@ -100,7 +104,7 @@ def download_chunk(chunk, directory):
 
     return image_paths
 
-def download_image(url, directory, name):
+def download_image(url, directory, name, resize):
     image_path = os.path.join(directory, '{}.jpg'.format(name))
 
     try:
@@ -112,6 +116,10 @@ def download_image(url, directory, name):
         image_data = response.read()
         pil_image = Image.open(BytesIO(image_data))
         pil_image_rgb = pil_image.convert('RGB')
+
+        if resize is not None:
+            pil_image_rgb = pil_image_rgb.resize(resize)
+
         pil_image_rgb.save(image_path, format='JPEG', quality=90)
 
         return image_path
@@ -127,8 +135,8 @@ def print_data_stats(data):
     print(series.describe())
 
 if '__main__' == __name__:
-    trainX, trainY = load_train_dataset()
-    testX, testY = load_test_dataset()
+    trainX, trainY = load_train_dataset(resize=(128, 128), sample=True)
+    testX, testY = load_test_dataset(resize=(128, 128), sample=True)
 
     print('train stats')
     print_data_stats(trainY)
