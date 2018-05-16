23f66ae5634920577f3a0511576ec61db1726069
Tu Hoang <hatu87@gmail.com>
2018-05-16 11:29:34 +0200

    add progress bar

--- google_landmarks_dataset.py
+++ google_landmarks_dataset.py
@@ -6,15 +6,16 @@ import os
 import tensorflow as tf
 import multiprocessing
 from functools import reduce, partial
-from tqdm import trange, tqdm
-
+from tqdm import tqdm
+from time import sleep
+import math
 #import keras.preprocessing.image as krimage
 
 DEFAULT_DATA_DIRECTORY='./data/landmarks_recognition/'
 TRAIN_DEFAULT_DIRECTORY='./data/landmarks_recognition/'
 TEST_DEFAULT_DIRECTORY = './data/landmarks_recognition/test/'
 DEFAULT_ORIGINAL_IMG_DIRECTORY='original'
-NUMBER_OF_SAMPLES = 5
+NUMBER_OF_SAMPLES = 1000
 
 def load_data(directory, resize):
     if resize is None:
@@ -59,7 +60,7 @@ def init_dataset(directory=DEFAULT_DATA_DIRECTORY, sample = False, resize=None, 
             os.makedirs(each)
 
     # load original train & test data
-    print('loading dataframes')
+    print('LOADING DATAFRAMES')
     train_df_path = os.path.join(directory, 'train.csv')
     test_df_path= os.path.join(directory, 'test.csv')
 
@@ -70,19 +71,24 @@ def init_dataset(directory=DEFAULT_DATA_DIRECTORY, sample = False, resize=None, 
         train_df = train_df.head(NUMBER_OF_SAMPLES)
         test_df = test_df.head(NUMBER_OF_SAMPLES)
 
-    print('downloading data')
+    print('DOWNLOADING DATA')
     if force_download:
         # download train
+        print('DOWNLOADING TRAIN DATA')
         download(train_directory, train_df, resize)
         # download test
+        print('DOWNLOADING TEST DATA')
         download(test_directory, test_df, resize)
 
     # build indexed files for train & test (remove missing data)
-    print('building index files')
+    print('BUILDING INDEX FILES')
     indexed_train_df_path = os.path.join(images_directory, 'train_index.csv')
     indexed_test_df_path = os.path.join(images_directory, 'test_index.csv')
 
+    print('BUILDING TRAIN INDEX FILE')
     indexed_train_df = build_indexed_file(train_directory, train_df, indexed_train_df_path)
+
+    print('BUILDING TEST INDEX FILE')
     indexed_test_df = build_indexed_file(test_directory, test_df, indexed_test_df_path)
 
     return indexed_train_df, indexed_test_df
@@ -99,6 +105,7 @@ def build_indexed_file(images_directory, data, indexed_path):
 
     for each in image_paths:
         img_series = img_series.append(pd.Series(each))
+        print('')
     img_series = img_series.rename('path')
 
     data['path'] = img_series
@@ -109,12 +116,15 @@ def build_indexed_file(images_directory, data, indexed_path):
 
 def check_chunk(chunk, directory):
     chunk_index, data = chunk
-    print('start checking chunk: {}'.format(chunk_index))
+    # print('start checking chunk: {}'.format(chunk_index))
 
     total_images = len(data)
     count = 0
     image_paths = {}
 
+    # tqdm.pandas(desc='{}'.format(chunk_index))
+    pbar = tqdm(desc='chunk {}'.format(chunk_index), total=total_images, position=chunk_index)
+    # with tqdm(total=total_images, position=chunk_index) as pbar:
     for index, row in data.iterrows():
         cls = row.get('landmark_id')
         if cls is None:
@@ -128,14 +138,17 @@ def check_chunk(chunk, directory):
             image_paths[index] = ''
 
         count += 1
-        print('[{}] - checked {} / {} image - {:4.4f}%'.format(chunk_index,
-                                                                count, total_images, count/total_images*100))
+        # data.progress_apply(lambda x: x + 1)
+        pbar.update(1)
+            # print('[{}] - checked {} / {} image - {:4.4f}%'.format(chunk_index,
+                                                                    # count, total_images, count/total_images*100))
 
+    pbar.close()
     return image_paths
 
 def split_chunks(data, num_processes):
     total_images = data.shape[0]
-    chunk_size = int(total_images / num_processes) if total_images > num_processes else 1
+    chunk_size = math.ceil(total_images / num_processes) if total_images > num_processes else 1
     chunks_data = [data.iloc[i:i + chunk_size] for i in range(0, total_images, chunk_size)]
     chunks = [(i, chunks_data[i]) for i in range(len(chunks_data))]
 
@@ -143,14 +156,12 @@ def split_chunks(data, num_processes):
     print('total number of images: {:,}'.format(total_images))
     print('chunk size: {:,}'.format(chunk_size))
     print('total chunks: {}'.format(len(chunks)))
-    # print('data: {}'.format(data))
-    # print('first chunk: {}'.format(chunks[0]))
 
     sum_chunk = 0
     for i in range(0, len(chunks)):
         print('chunk {} length: {}'.format(chunks[i][0], len(chunks[i][1])))
         sum_chunk += len(chunks[i][1])
-    # sum_chunk = reduce(lambda x, acc: acc + len(x), chunks)
+
     print('sum chunks: {:,}'.format(sum_chunk))
 
     assert(sum_chunk == data.shape[0])
@@ -168,29 +179,26 @@ def download(directory, data, resize):
 
     for each in image_paths:
         img_series = img_series.append(pd.Series(each))
+        print('')
     img_series = img_series.rename('path')
 
     return img_series
 
 def download_chunk(chunk, directory, resize):
     chunk_index, data = chunk
-    # print('start downloading chunk: {}'.format(chunk_index))
     total_images = len(data)
-    count = 0
     image_paths = {}
 
-    # tqdm.pandas
-    with tqdm(total = total_images, position=chunk_index) as pbar:
-        for index, row in data.iterrows():
-            cls = None if 'landmark_id' not in data.columns else str(row['landmark_id'])
-            image_path = download_image(row['url'], directory, index, resize, cls)
-            if image_path != '':
-                image_paths[index] = image_path
+    pbar = tqdm(desc='chunk {}'.format(chunk_index), total=total_images, position=chunk_index)
+    for index, row in data.iterrows():
+        cls = None if 'landmark_id' not in data.columns else str(row['landmark_id'])
+        image_path = download_image(row['url'], directory, index, resize, cls)
+        if image_path != '':
+            image_paths[index] = image_path
 
-            count += 1
-            pbar.update(1)
-            # print('[{}] - download {} / {} image - {:4.4f}%'.format(chunk_index, count, total_images, count/total_images*100))
+        pbar.update(1)
 
+    pbar.close()
     return image_paths
 
 def download_image(url, directory, name, resize, cls):
@@ -202,7 +210,6 @@ def download_image(url, directory, name, resize, cls):
 
     try:
         if os.path.exists(image_path):
-            # print('Image {} already exists. Skipping download.'.format(image_path))
             return image_path
 
         response = request.urlopen(url)
@@ -224,4 +231,4 @@ def download_image(url, directory, name, resize, cls):
         return ''
 
 if '__main__' == __name__:
-    train_df, test_df = init_dataset(directory=DEFAULT_DATA_DIRECTORY, sample=False, resize=(128, 128), force_download=True)
+    train_df, test_df = init_dataset(directory=DEFAULT_DATA_DIRECTORY, sample=True, resize=(128, 128), force_download=True)
